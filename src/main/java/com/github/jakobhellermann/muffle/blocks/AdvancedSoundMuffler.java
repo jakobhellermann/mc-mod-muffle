48c245df54b39aabd005ad175277941e71589628
Jakob Hellermann <jakob.hellermann@protonmail.com>
2020-06-15 11:44:09 +0200

    save range and blocklist in NBT, check if should block in mixin
    Signed-off-by: Jakob Hellermann <jakob.hellermann@protonmail.com>

--- src/main/java/com/github/jakobhellermann/muffle/blocks/AdvancedSoundMuffler.java
+++ src/main/java/com/github/jakobhellermann/muffle/blocks/AdvancedSoundMuffler.java
@@ -5,9 +5,17 @@ import com.github.jakobhellermann.muffle.blockentities.AdvancedSoundMufflerBlock
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class AdvancedSoundMuffler extends Block implements BlockEntityProvider {
     public static final Identifier ID = new Identifier(Muffle.MOD_ID, "advanced_sound_muffler");
@@ -19,5 +27,19 @@ public class AdvancedSoundMuffler extends Block implements BlockEntityProvider {
     @Override
     public BlockEntity createBlockEntity(BlockView view) {
         return new AdvancedSoundMufflerBlockEntity();
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        AdvancedSoundMufflerBlockEntity blockEntity = (AdvancedSoundMufflerBlockEntity) world.getBlockEntity(pos);
+
+        assert blockEntity != null;
+        System.out.println("Range: " + blockEntity.getRange());
+        blockEntity.setRange(blockEntity.getRange() + 1);
+
+        int size = Registry.SOUND_EVENT.getIds().size();
+        System.out.println("sound event reg size: " + size);
+
+        return super.onUse(state, world, pos, player, hand, hit);
     }
 }
